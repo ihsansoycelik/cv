c70bd85c1a09c596474a0abc5bd0e261310e934a
Ihsan Soyçelik <44748435+ihsansoycelik@users.noreply.github.com>
2026-02-24 16:53:48 +0300

    test image instead of video

--- app/page.tsx
+++ app/page.tsx
@@ -152,7 +152,7 @@ export default function Personal() {
           {PROJECTS.map((project) => (
             <div key={project.name} className="space-y-2">
               <div className="relative rounded-2xl bg-zinc-50/40 p-1 ring-1 ring-zinc-200/50 ring-inset dark:bg-zinc-950/40 dark:ring-zinc-800/50">
-                <ProjectVideo src={project.video} />
+                <img src="/cover.jpg" />
               </div>
               <div className="px-1">
                 <a
